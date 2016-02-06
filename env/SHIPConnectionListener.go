f31b3f768fc9a5c914ec58aaa29ddfd6c0beff85
LukeusMaximus <lukeusmaximus39@gmail.com>
2016-02-06 20:51:20 +0000

    Changed the connection manager's name to listener to reflect its change in responsibility.
    The connection lister just listens for new connections concurrently and sends messages out on a channel when new connections are made.

---
+++ env/SHIPConnectionListener.go
@@ -0,0 +1,50 @@
+// SHIPConnectionListener
+package env
+
+import (
+	"fmt"
+	"net"
+)
+
+type SCLInfoType int
+
+const (
+	SCL_LISTEN_SUCCESS = iota
+	SCL_LISTEN_FAILED
+	SCL_NEW_SHIP_CONNECTION
+)
+
+type SCLInfo struct {
+	msgType        SCLInfoType
+	err            error
+	shipConnection *SHIPConnectionHandler
+}
+
+type SHIPConnectionListener struct {
+	info chan SCLInfo
+}
+
+func (cm *SHIPConnectionListener) Start(port uint16) {
+	cm.info = make(chan SCLInfo)
+
+	go cm.listen(port)
+}
+
+func (cm *SHIPConnectionListener) listen(port uint16) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		cm.info <- SCLInfo{msgType: SCL_LISTEN_FAILED, err: err}
+		return
+	}
+	cm.info <- SCLInfo{msgType: SCL_LISTEN_SUCCESS}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			cm.info <- SCLInfo{msgType: SCL_LISTEN_FAILED, err: err}
+			return
+		}
+		handler := new(SHIPConnectionHandler)
+		handler.Start(conn)
+		cm.info <- SCLInfo{msgType: SCL_NEW_SHIP_CONNECTION, shipConnection: handler}
+	}
+}
